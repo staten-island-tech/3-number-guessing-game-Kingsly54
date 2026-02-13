d74241e592bea89dc9ff870e463d2d0d3c1bf66c
Kingsly54 <kingslyw@nycstudents.net>
2026-02-12 20:35:48 -0500

    On my way!

---
+++ app.py
@@ -0,0 +1,21 @@
+
+import random
+print("You got it!")
+ask=int(input("What is your number"))
+vvvvv=int(random.randint(1,1000))
+while ask!=vvvvv:
+       ask=int(input("Try again"))
+
+
+print("You got it!")
+
+        
+                
+        
+
+
+        
+       
+
+        
+
