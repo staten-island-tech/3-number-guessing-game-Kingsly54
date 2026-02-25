d95e0faa073e93c33083d7b087f4bc4e5b96d656
Kingsly54 <kingslyw@nycstudents.net>
2026-02-25 09:47:25 -0500

    hhh

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 
 import random
-vvvvv=random.randint(1,10)
-print("You got it!")
+vvvvv=random.randint(1,1000)
 ask=int(input("What is your number"))
+guesshistory=[]
 while ask!=vvvvv:
+       print(guesshistory)
        if ask<vvvvv:
               ask=int(input("try again HIGHER"))
+              guesshistory.append(ask)
        if ask>vvvvv:
               ask=int(input("try again LOWER"))
+              guesshistory.append(ask)
+print(f"You got it! Your previous guesses:{guesshistory}")
 
-print("You got it")
-       
+
 
 
                 
